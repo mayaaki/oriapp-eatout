e8a0113a2d9234d3b3ecd0a06dd087adc9032930
mayaaki <ma.k12ad8@gmail.com>
2023-08-31 13:40:00 +0900

    日付設定のジャバスクリプト

---
+++ app/javascript/date.js
@@ -0,0 +1,9 @@
+$('#first_date').datepicker({
+  /*日付フォーマット*/
+  dateFormat: 'yy-mm-dd'
+});
+
+$('#last_date').datepicker({
+  /*日付フォーマット*/
+  dateFormat: 'yy-mm-dd'
+});
--- app/models/budget.rb
+++ app/models/budget.rb
@@ -2,5 +2,13 @@ class Budget < ApplicationRecord
   belongs_to :user
   # has_many :records
 
-  validates :first_date, :last_date, :budgets, :users, presence: true
+  validates :first_date, :last_date, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/ }
+  validates :budgets, :users, presence: true
+  validate :start_end_check
+
+  def start_end_check
+    if first_date.present? && last_date.present? && first_date >= last_date
+      errors.add(:last_date, "cannot precede the start date.")
+    end
+  end
 end
