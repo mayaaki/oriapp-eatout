e6f245758b3fb842a969fa9a4b035fee62d14c6a
mayaaki <ma.k12ad8@gmail.com>
2023-09-04 15:16:47 +0900

    レコードテーブルバリデーションの設定１

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -13,6 +13,18 @@ class RecordsController < ApplicationController
   end
 
   def create
+    @budget = Budget.find(params[:budget_id])
+    @record = Record.new(record_params)
+    if @record.save
+      redirect_to budget_records_path(@budget.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
+  private
+
+  def record_params
+    params.require(:record).permit(:recording_date, :restaurant_name, :price, :prefecture_id, :city, :situation_id, :genre_id, :url).merge(budget_id: params[:budget_id])
+  end
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -2,7 +2,7 @@ class Record < ApplicationRecord
 
   with_options presence: true do
     validates :recording_date, format: { with: /\A\d{4}-\d{2}-\d{2}\z/ }
-    validates :restaurant_name
+    validates :restaurant_name, length: { maximum: 20 }
     validates :price, numericality: { only_integer: true }
     validates :city, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   end
@@ -10,6 +10,16 @@ class Record < ApplicationRecord
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :situation_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :genre_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  validate :date_check
+
+  def date_check
+    
+    binding.pry
+    if  budget.first_date < recording_date  && budget.last_date > recording_date 
+      errors.add(:recording_date, "is out of range.")
+    end
+  end
 
   belongs_to :budget
   # has_one :favorite
